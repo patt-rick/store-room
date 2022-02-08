decd92267737a51760a30929969e1b0514b45dd0
Minnez <patrickackom003@gmail.com>
2022-02-08 10:18:51 +0000

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="main">
+    <h1>this is where i start from</h1>
+    <h3>we go get there. </h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
