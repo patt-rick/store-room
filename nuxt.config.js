4ac2c43835b493eae587b29e8857a3bdd0e67fda
minnez <patrickackom003@gmail.com>
2022-09-19 16:24:45 +0000

    added fonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,20 +8,24 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,200&display=swap',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '~assets/css/main.css'
-  ],
+  css: ['~assets/css/main.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: {
-    dirs:[
+    dirs: [
       '~/components',
       '~/components/cards',
       '~/components/charts',
@@ -29,22 +33,15 @@ export default {
       '~/components/forms',
       '~/components/navigations',
       '~/components/tables',
-      '~/components/modals'
-
-    ]
+      '~/components/modals',
+    ],
   },
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    '@nuxtjs/style-resources'
-  ],
-
+  buildModules: ['@nuxtjs/style-resources'],
 
   styleResources: {
-    scss: [
-      '~assets/scss/mixins.scss',
-      '~assets/scss/variables.scss'
-    ]
+    scss: ['~assets/scss/mixins.scss', '~assets/scss/variables.scss'],
   },
 
   // Modules: https://go.nuxtjs.dev/config-modules
